6fbf0c6cc4bc209df2d3e1f17153439a34a46533
Margot BIGNON <margot.bignon78@gmail.com>
2023-12-26 15:55:22 +0100

    Add function show in ProgramController

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 Class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Donkey Séries',
+         ]);
+    }
+
+    #[Route('/{id<\d+>}', methods:["GET"], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
          ]);
     }
 }
