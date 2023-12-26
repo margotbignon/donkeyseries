783f0c7352050767e1edeae72f11d7f5b5f2090a
Margot BIGNON <margot.bignon78@gmail.com>
2023-12-26 16:56:43 +0100

    View programs and categories

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,8 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,18 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 Class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Donkey Séries',
+            'programs' => $programs,
          ]);
     }
 
     #[Route('/{id<\d+>}', methods:["GET"], name: 'show')]
-    public function show(int $id): Response
+    public function show(Program $program): Response
     {
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
          ]);
     }
 }
