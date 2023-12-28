63b93aa6cb2893957d63e7fb04fc5d5203d76964
Margot BIGNON <margot.bignon78@gmail.com>
2023-12-28 12:25:23 +0100

    add view show episode

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Repository\ProgramRepository;
@@ -10,6 +11,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 
 #[Route('/program', name: 'program_')]
 Class ProgramController extends AbstractController
@@ -33,18 +36,26 @@ Class ProgramController extends AbstractController
         ]);
     }
 
-    /*#[Route('/{program.id<\d+>}/seasons/{season.id<\d+>}', name: 'season_show')]
-    public function showSeason(Program $program, Season $season): Response
+    #[Route('/{idProgram<\d+>}/seasons/{idSeason<\d+>}', name: 'season_show')]
+    public function showSeason(#[MapEntity(id: 'idProgram')] Program $program, #[MapEntity(id : 'idSeason')] Season $season): Response
     {
-
-
         return $this->render('program/season_show.html.twig', [
             'program' => $program,
             'season' => $season,
         ]);
-    }*/
+    }
 
-    #[Route('/{idProgram<\d+>}/seasons/{idSeason<\d+>}', methods:["GET"], name: 'season_show')]
+    #[Route('/{idProgram<\d+>}/seasons/{idSeason<\d+>}/episode/{idEpisode<\d+>}', name: 'episode_show')]
+    public function showEpisode(#[MapEntity(id: 'idProgram')] Program $program, #[MapEntity(id : 'idSeason')] Season $season, #[MapEntity(id : 'idEpisode')] Episode $episode): Response
+    {
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode
+        ]);
+    }
+
+    /*#[Route('/{idProgram<\d+>}/seasons/{idSeason<\d+>}', methods:["GET"], name: 'season_show')]
     public function showSeason(int $idProgram, int $idSeason, EntityManagerInterface $em): Response
     {
 
@@ -59,5 +70,5 @@ Class ProgramController extends AbstractController
             'program' => $program,
             'season' => $season,
         ]);
-    }
+    }*/
 }
